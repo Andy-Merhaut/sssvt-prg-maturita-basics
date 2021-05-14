139fb68df80cdc492ebc136e504ab2590ee7c6a4
Andy-Merhaut <andreasmerhaut11502@gmail.com>
2021-05-14 14:06:03 +0200

    basics 1

--- TenMaturitaBasics/Program.cs
+++ TenMaturitaBasics/Program.cs
@@ -10,6 +10,45 @@ namespace TenMaturitaBasics
     {
         static void Main(string[] args)
         {
+            char znak = 'a';
+            string retezec;
+
+            retezec = znak.ToString();
+            retezec = Convert.ToString(znak);
+
+            string[] poleRetezcu = new string[1];
+            poleRetezcu[0] = retezec;
+
+            List<string> kolekceRetezcu = new List<string>();
+            kolekceRetezcu.Add(retezec);
+
+            int cislo = 1;
+            string konverzniCislo = "1";
+
+            cislo = Convert.ToInt32(konverzniCislo);
+            cislo = int.Parse(konverzniCislo);
+
+            double desetineCislo = 1.2d;
+            float cisloSPohyblivouCarkou = 1.2f;
+
+            var dbl = 1.2d;
+            var flt = 1.2f;
+
+            String s;
+            string s1;
+
+            Int32 i;
+            int i1;
+
+            Single f;
+            float f1;
+
+            int[] poleCisel;
+            List<int> kolekceCisel;
+            
+
+            decimal extraDesetineCislo;
+            
         }
     }
 }
